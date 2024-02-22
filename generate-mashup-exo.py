0f2d7b44c9febdda988a7dab3a9ca932000ceec5
xszqxszq <943551369@qq.com>
2024-02-22 10:01:28 +0800

    Generate exo for mashup

---
+++ generate-mashup-exo.py
@@ -0,0 +1,72 @@
+import audiofile
+import rpp
+import tqdm
+import ffmpeg
+from rpp import Element
+
+mashup_audio = r'D:\Temp\mashup.flac'
+mashup_project = r'D:\Temp\mashup.rpp'
+
+video_w = 1920
+video_h = 1080
+fps = 60
+w = 8
+h = 8
+item_w, item_h = video_w / w, video_h / h
+x1 = -w // 2 * item_w + ((item_w / 2) if w % 2 == 0 else 0)
+y1 = -h // 2 * item_h + ((item_h / 2) if h % 2 == 0 else 0)
+
+exo = '''[exedit]
+width={}
+height={}
+rate={}
+scale=1
+length={}
+audio_rate=48000
+audio_ch=2'''.format(video_w, video_h, fps, int(fps * audiofile.duration(mashup_audio)) + 100)
+
+video = '''
+[{0}]
+start={1}
+end={2}
+layer={9}
+group={9}
+overlay=1
+camera=0
+[{0}.0]
+_name=视频文件
+播放位置={3}
+播放速度={4:.2f}
+循环播放=0
+读取Alpha通道=0
+file={5}
+[{0}.1]
+_name=标准属性
+X={6:.1f}
+Y={7:.1f}
+Z=0.0
+缩放率={8:.2f}
+透明度=0.0
+旋转=0.00
+blend=0'''
+
+r = rpp.load(open(mashup_project, 'r', encoding='UTF-8'))
+
+tracks = [i for i in r.children if isinstance(i, Element) and i.tag == 'TRACK']
+for index, track in tqdm.tqdm(enumerate(tracks)):
+	items = [i for i in track.children if isinstance(i, Element) and i.tag == 'ITEM']
+	for item in items:
+		path = [i for i in item.children if isinstance(i, Element) and i.tag == 'SOURCE'][0].children[0][1]
+		info = [stream for stream in ffmpeg.probe(path)['streams'] if stream['codec_type'] == 'video'][0]
+		width, height, now_fps = info['width'], info['height'], int(info['r_frame_rate'].split('/')[0]) / int(info['r_frame_rate'].split('/')[1])
+		pos = int(float([i for i in item.children if isinstance(i, list) and i[0] == 'POSITION'][0][1]) * fps)
+		start = int(float([i for i in item.children if isinstance(i, list) and i[0] == 'SOFFS'][0][1]) * now_fps)
+		length = int(float([i for i in item.children if isinstance(i, list) and i[0] == 'LENGTH'][0][1]) * fps)
+		speed = float([i for i in item.children if isinstance(i, list) and i[0] == 'PLAYRATE'][0][1]) * 100
+		scale = item_w / width * 100
+
+		x = x1 + ((index - 1) % w) * item_w
+		y = y1 + ((index - 1) // w) * item_h
+		exo += video.format(index - 1, pos, pos + length, start, speed, path, x, y, scale, index)
+
+open('result.exo', 'w', encoding='gbk').write(exo)
